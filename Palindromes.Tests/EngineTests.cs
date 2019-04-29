eceb6c92c198cab46858018dd543ef439bd142e3
Zbigniew Kosior <zkosior@users.noreply.github.com>
2019-04-29 23:12:28 +0200

    First implementation.

--- Palindromes.Tests/EngineTests.cs
+++ Palindromes.Tests/EngineTests.cs
@@ -1,12 +1,12 @@
 namespace Palindromes.Tests
 {
-	using System;
 	using Xunit;
 
 	public class EngineTests
 	{
 		[Theory]
-		[InlineData("aasdfgfdsaf", 0, 9, 1, 8)]
+		[InlineData("aasdfgfdsaf", 0, 10, 1, 9)]
+		[InlineData("aaasdfgfdsaff", 1, 11, 2, 10)]
 		public void FindsLargestPalindromeWithCenterBetweenGivenRanges(
 			string input,
 			int startIndex,
--- Palindromes/Engine.cs
+++ Palindromes/Engine.cs
@@ -1,15 +1,30 @@
 namespace Palindromes
 {
-	using System;
+#pragma warning disable SA1008 // Opening parenthesis must be spaced correctly
 
-	public class Engine
+	public static class Engine
 	{
-		public static Tuple<int, int> FindLargestPalindrome(
+		public static (int, int) FindLargestPalindrome(
 			string input,
 			int startIndex,
 			int endIndex)
 		{
-			return Tuple.Create(1, 8);
+			var middle = (endIndex + startIndex) / 2;
+			int i = middle - 1;
+			int j = middle + 1;
+			for (; i >= startIndex; i--, j++)
+			{
+				if (input[i] != input[j])
+				{
+					i++;
+					j--;
+					break;
+				}
+			}
+
+			return (i, j);
 		}
 	}
+
+#pragma warning restore SA1008 // Opening parenthesis must be spaced correctly
 }
