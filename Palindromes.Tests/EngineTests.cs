9812c999b62e86c9eafab4fd540abd816d2b36f7
Zbigniew Kosior <zkosior@users.noreply.github.com>
2019-04-30 12:21:46 +0200

    Added searching for first found palindrome in given input. Added empty string checks.

--- Palindromes.Tests/EngineTests.cs
+++ Palindromes.Tests/EngineTests.cs
@@ -15,11 +15,10 @@ namespace Palindromes.Tests
 			int palindromeStart,
 			int palindromeEnd)
 		{
-			var result = Engine
-				.FindLargestPalindrome(input, startIndex, endIndex);
+			var result = Engine.FindLargestPalindrome(input, startIndex, endIndex);
 
-			Assert.Equal(palindromeStart, result.Value.Item1);
-			Assert.Equal(palindromeEnd, result.Value.Item2);
+			Assert.Equal(palindromeStart, result.Value.Start);
+			Assert.Equal(palindromeEnd, result.Value.End);
 		}
 
 		[Theory]
@@ -29,13 +28,41 @@ namespace Palindromes.Tests
 		[InlineData("abc", 0, 2)]
 		[InlineData("abc", 1, 1)]
 		[InlineData("abcde", 1, 3)]
+		[InlineData("", 0, 0)]
+		[InlineData(" ", 0, 0)]
+		[InlineData("   ", 0, 2)]
 		public void WhenNoPalindromeReturnsNull(
 			string input,
 			int startIndex,
 			int endIndex)
 		{
-			var result = Engine
-				.FindLargestPalindrome(input, startIndex, endIndex);
+			var result = Engine.FindLargestPalindrome(input, startIndex, endIndex);
+
+			Assert.Null(result);
+		}
+
+		[Theory]
+		[InlineData("aasdfgfdsaf", 1, 9)]
+		[InlineData("aasdfgfdsaffff", 1, 9)]
+		public void FindsPalindromeOutsideCenter(
+			string input,
+			int palindromeStart,
+			int palindromeEnd)
+		{
+			(int start, int end) = Engine.FindPalindrome(input).Value;
+
+			Assert.Equal(palindromeStart, start);
+			Assert.Equal(palindromeEnd, end);
+		}
+
+		[Theory]
+		[InlineData("abcdefghijk")]
+		[InlineData("a")]
+		[InlineData("")]
+		public void WhenNoPalindromeAtAllReturnsNull(
+			string input)
+		{
+			var result = Engine.FindPalindrome(input);
 
 			Assert.Null(result);
 		}
--- Palindromes/Engine.cs
+++ Palindromes/Engine.cs
@@ -5,11 +5,12 @@ namespace Palindromes
 
 	public static class Engine
 	{
-		public static (int, int)? FindLargestPalindrome(
+		public static (int Start, int End)? FindLargestPalindrome(
 			string input,
 			int startIndex,
 			int endIndex)
 		{
+			if (string.IsNullOrWhiteSpace(input)) return null;
 			if (startIndex == endIndex) return null;
 
 			var middle = (endIndex + startIndex) / 2;
@@ -28,6 +29,23 @@ namespace Palindromes
 
 			if (i >= j) return null;
 			return (i, j);
+		}
+
+		public static (int Start, int End)? FindPalindrome(string input)
+		{
+			if (string.IsNullOrWhiteSpace(input)) return null;
+			var zero = FindLargestPalindrome(input, 0, input.Length - 1);
+			if (zero.HasValue) return zero.Value;
+
+			for (int i = 1; i < input.Length - 2; i++)
+			{
+				var left = FindLargestPalindrome(input, 0, input.Length - 1 - i);
+				if (left.HasValue) return left.Value;
+				var right = FindLargestPalindrome(input, i, input.Length - 1);
+				if (right.HasValue) return right.Value;
+			}
+
+			return null;
 		}
 	}
 
